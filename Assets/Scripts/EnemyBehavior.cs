970f259e374181eca0d1a5c6753a1c91816b6abe
Chester193 <46018874+Chester193@users.noreply.github.com>
2020-11-25 14:02:51 +0100

    #2 spawning enemies endlessly

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    float randX, randY;
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Launch()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        randX = Random.Range(-100f, 100f);
+        randY = Random.Range(-100f, 100f);
+        rigidbody2d.AddForce(new Vector2(randX, randY));
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 1f;
+    float nextSpawn = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Launch();
+        }
+    }
+
+    void Launch()
+    {
+        whereToSpawn = new Vector2(transform.position.x, transform.position.y);
+        GameObject gameobject = Instantiate(enemy, whereToSpawn, Quaternion.identity);
+
+        EnemyBehavior enemyBehavior = gameobject.GetComponent<EnemyBehavior>();
+        enemyBehavior.Launch();
+
+    }
+}
