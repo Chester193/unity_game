0db16c694fa004782bb56682f16045d713086ae9
Damian Jacewicz <215739@edu.p.lodz.pl>
2020-11-29 22:33:16 +0100

    level menager beginning

---
+++ Assets/Scripts/LevelMenager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenager : MonoBehaviour
+{
+    private int level;
+    private int experience;
+    private int experienceToNextLevel;
+
+    public LevelMenager() 
+    {
+        level = 0;
+        experience = 0;
+        experienceToNextLevel = 100;
+    }
+
+    public void AddExperience(int amount) 
+    {
+        experience += amount;
+
+        if (experience >= experienceToNextLevel)
+        {
+            experience -= experienceToNextLevel;
+            experienceToNextLevel = 0;
+            level++;
+        }
+    }
+}
