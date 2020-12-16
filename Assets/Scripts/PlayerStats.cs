cbf89ddaa4257cef6795ed53ec1d44512ceb76fc
Michal <michalogloza97@gmail.com>
2020-12-16 18:01:24 +0100

    Initial level manager

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public static class PlayerStats
 {
+    private static int experienceToNextLevel = 10;
+    private static int experienceAmount = 0;
+
     public static int Points { get; set; }
+    public static int Level { get; private set; }
+
+    public static float Exp()
+    {
+        return (float)experienceAmount / experienceToNextLevel;
+    }
+
+    public static void SavePoints()
+    {
+        experienceAmount += Points;
+        UpdateLevel();
+        Points = 0;
+    }
+
+    private static void UpdateLevel()
+    {
+        if (experienceAmount >= experienceToNextLevel)
+        {
+            experienceAmount -= experienceToNextLevel;
+            experienceToNextLevel = (int)((float)experienceToNextLevel * 2f * 0.72);
+            Level++;
+        }
+    }
 }
